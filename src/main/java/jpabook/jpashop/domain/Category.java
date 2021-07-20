accdd1a2f75543e9e969d46e7636f06a715879d9
JangJangYi <jj__yi@imsmobility.co.kr>
2021-07-20 17:13:19 +0900

    엔티티 클래스 개발 02(chapter02)

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item", //객체는 컬렉션, 컬렉션끼리 다대다관계를 맺을 수 있지만 테이블은 그렇지 않기 때문에 다대다관계 일때 테이블을 만들어야한다. 그래서 중간에 있는 테이블과 관계를 맺는 어노테이션이라고 생각하면 된다.
+        joinColumns = @JoinColumn(name = "category_id"),
+        inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
