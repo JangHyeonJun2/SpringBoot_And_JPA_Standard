4af95ee05b6fbf2847bcaaeca67f835a363aac85
JangJangYi <jj__yi@imsmobility.co.kr>
2021-07-21 17:43:05 +0900

    회원 도메인 개발(chapter03) - 회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,15 +2,17 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+
+    private final EntityManager em;
+
+    public MemberRepository(EntityManager em) {
+        this.em = em;
+    }
 
     public void save(Member member) {
         em.persist(member);
