d84cf9b62fa910130fd587eadc4afd0445faa5cd
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-07-29 00:43:47 +0900

    OrderRepository 작성 및 OrderService 작성

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final
+
+    //주문
+
+    //취소
+
+    //검색
+}
