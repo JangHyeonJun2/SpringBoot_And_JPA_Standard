e46e32373324c9ebdd021a52fe0410c24624ccad
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-08-17 01:09:17 +0900

    주문 내역확인하기 Controller 와 HTML 작성

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -4,6 +4,7 @@ import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,7 +59,7 @@ public class OrderService {
     }
 
     //검색
-//    public List<Order> findOrder(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findOrder(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
