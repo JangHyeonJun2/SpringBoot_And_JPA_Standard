2438c9b0c482c2e0224e41b3f23a2f2b597d97c3
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-08-23 01:48:46 +0900

    [JPA 활용2 API 개발과 성능 최적화] - 회원등록 API

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {//@RequestBody는 json으로 온 데이터를 entity에 맵핑을 자동으로 해줌 (json말고 다른 것도 세팅 가능)
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {//@RequestBody는 json으로 온 데이터를 entity에 맵핑을 자동으로 해줌 (json말고 다른 것도 세팅 가능)
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+
+        private Long id;
+    }
+}
