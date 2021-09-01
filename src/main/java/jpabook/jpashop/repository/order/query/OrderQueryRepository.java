01ecec8f1948c0f6a426d229b7b316a28127613f
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-09-01 16:54:09 +0900

    [JPA 활용 2] - JPA에서 DTO 직접 조회(컬렉션 조회 최적화)

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,6 +20,36 @@ public class OrderQueryRepository {
             orderQueryDto.setOrderItems(orderItems);
         });
         return result;
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        List<Long> orderIds = toOrderIds(result);
+
+        // TODO findOrderItemMap 이름으로 메서드 만들어주기
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+        "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, oi.item.name, oi.orderPrice, oi.count)" +
+                " from OrderItem oi" +
+                " join oi.item i" +
+                " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        /**
+         * findOrderQueryDtos()랑 차이점은 indOrderQueryDtos()는 루프를 돌릴 때마다 쿼리를 날렸지만, 위에 orderItems에서 쿼리를 한번 날리고 메모리에 Map을 다 가져온 다음에 매칭하여 값을 세팅해준다. 이렇게 되면 여기에서는 findOrders(), orderItems에서 총 2번의 쿼리가 나간다.
+         */
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));//orderId를 기준으로 맵으로 바꿀 수 있다.
+
+        result.forEach(orderQueryDto -> orderQueryDto.setOrderItems(orderItemMap.get(orderQueryDto.getOrderId())));
+        return result;
+
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        List<Long> orderIds = result.stream().map(orderQueryDto -> orderQueryDto.getOrderId()).collect(Collectors.toList());
+        return orderIds;
     }
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
